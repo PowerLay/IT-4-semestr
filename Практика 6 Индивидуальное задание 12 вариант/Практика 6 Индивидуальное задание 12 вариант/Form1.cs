575c07a0b1fb91ad381dc61b0cf98189e6efa196
PowerLay <34715449+PowerLay@users.noreply.github.com>
2021-03-10 20:38:14 +0300

    Практика 6

---
+++ Практика 6 Индивидуальное задание 12 вариант/Практика 6 Индивидуальное задание 12 вариант/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace _2
+{
+    public partial class Form1 : Form
+    {
+        private readonly int[] Mas = new int[15];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var rand = new Random();
+            textBox1.Text = "";
+            for (var i = 0; i < 15; i++)
+            {
+                Mas[i] = rand.Next(-50, 50);
+                textBox1.Text += "Mas[" + Convert.ToString(i) + "] = "
+                                 + Convert.ToString(Mas[i]) + Environment.NewLine;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var average = Mas.Average();
+            var geomAverage = Math.Pow(Math.Abs(Mas.Aggregate((x, y) => x * y)), 1.0 / Mas.Length);
+            textBox2.Text = $"Среднее арифметическое значение = {average}" + Environment.NewLine;
+            textBox2.Text += $"Среднее геометрическое значение = {geomAverage}" + Environment.NewLine;
+        }
+    }
+}
