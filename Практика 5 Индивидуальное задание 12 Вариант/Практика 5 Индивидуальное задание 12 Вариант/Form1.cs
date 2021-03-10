e1c05d58e5e1c8ed2f5b2938e0c8edde384ea231
PowerLay <34715449+PowerLay@users.noreply.github.com>
2021-03-10 20:39:46 +0300

    5 практика

---
+++ Практика 5 Индивидуальное задание 12 Вариант/Практика 5 Индивидуальное задание 12 Вариант/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace _12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var str = listBox1.Items[listBox1.SelectedIndex].ToString();
+
+            str = str.Replace(" ", "");
+
+            label1.Text = "Строка: \"" + str + "\"";
+        }
+    }
+}
